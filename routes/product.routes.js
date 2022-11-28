5c1b6063583efc96b465325e1c91f36502791534
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-11-28 16:43:35 -0300

    feat: include put product route

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,35 +1,49 @@
 import { Router } from "express";
 import Product from "../models/Product.model.js";
 
+const router = Router();
 
-const router = Router()
+router.post("/product", async (req, res, next) => {
+  const { productName, image, price, description } = req.body;
 
-router.post('/product', async (req, res, next) => {
-    const {productName, image, price, description } = req.body
-
-    try{
-        const newProduct = await Product.create({
-            productName,
-            image,
-            price,
-            description,
-            owner: req.user.id,
-        })
-        res.status(200).json(newProduct)
-    } catch (error) {
-        next(error)
-    }
-} )
-
-router.get('/product', async (req, res, next) => {
-    try{
-        const userId = req.user.id
-        const allProduct = await Product.find({user: userId});
-        res.status(200).json(allProduct)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
+  try {
+    const newProduct = await Product.create({
+      productName,
+      image,
+      price,
+      description,
+      owner: req.user.id,
+    });
+    res.status(200).json(newProduct);
+  } catch (error) {
+    next(error);
+  }
 });
 
+router.get("/product", async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const allProduct = await Product.find({ user: userId });
+    res.status(200).json(allProduct);
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default router
+router.put("/product/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+  const { productName, image, price, description } = req.body;
+  try {
+    const updateProduct = await Product.findOneAndUpdate({ _id: id, user: userId }, req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
