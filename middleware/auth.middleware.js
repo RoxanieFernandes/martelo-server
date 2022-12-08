66cea056631cbc5bb5d168579ad5ff6d01a564cc
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-12-08 11:01:42 -0300

    try fix error

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,18 +2,18 @@ import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-  try {
-    if (!authorization) {
-      const error = new Error("Request without token");
-      throw error;
-    }
-    const [, token] = authorization.split(' ')[1];
 
+  if (!authorization) {
+    const error = new Error("Request without token");
+    throw error;
+  }
+  const [, token] = authorization.split(" ")[1];
+  try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { ...decodedToken };
     next();
   } catch (error) {
-    res.status(401).json({message: error.message})
+    res.status(401).json({ message: error.message });
   }
 };
 
